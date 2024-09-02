29e860f8499fbb9f3794c3b388153ab49dcc0574
okawibawa <okaa.wibawa@gmail.com>
2024-09-03 02:00:55 +0800

    refactor: auth, dashboard, and orders ui wip

---
+++ app/(app)/orders/page.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export default function Orders() {
+  return (
+    <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+      <div className="flex items-center">
+        <h1 className="text-lg font-semibold md:text-2xl">Orders</h1>
+      </div>
+
+      <div
+        className="flex flex-shrink justify-center rounded-lg border border-dashed shadow-sm"
+        x-chunk="dashboard-02-chunk-1"
+      >
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Customer</TableHead>
+              <TableHead className="hidden md:table-cell">Date</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Liam Johnson</div>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-23</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Olivia Smith</div>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-24</TableCell>
+              <TableCell className="text-right">$150.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Noah Williams</div>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-25</TableCell>
+              <TableCell className="text-right">$350.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Emma Brown</div>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-26</TableCell>
+              <TableCell className="text-right">$450.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Liam Johnson</div>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-23</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Liam Johnson</div>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-23</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Olivia Smith</div>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-24</TableCell>
+              <TableCell className="text-right">$150.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Emma Brown</div>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-26</TableCell>
+              <TableCell className="text-right">$450.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    </main>
+  );
+}
